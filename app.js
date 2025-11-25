c246ab3f420919aa55f6fd69fbe5017d6bbe21f3
severabdullayeva <severabdullayeva>
2025-11-25 23:44:26 +0400

    new folder

---
+++ app.js
@@ -0,0 +1,16 @@
+//Birinci tapsiriq
+let fullName = "Sever Abdullayeva";
+console.log(fullName);
+console.log(fullName[0], fullName.split(" ")[1][0]);
+
+//ikinci tapsiriq
+console.log(fullName.split(" ")[1], fullName.split(" ")[0]);
+
+//Ucuncu tapsiriq
+let noSpace = fullName.replaceAll(" ", "");
+console.log(noSpace);
+console.log(noSpace.length);
+
+
+
+
