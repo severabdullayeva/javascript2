3fd842918b377e767bb341af352d3d3397c16043
severabdullayeva <severabdullayeva>
2025-11-27 16:22:51 +0400

    update

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ let fullName = "Sever Abdullayeva";
 console.log(fullName);
 console.log(fullName[0], fullName.split(" ")[1][0]);
 
+// let firstName = "Sever";
+// let lastName = "Abdullayeva"
+// let fullName = `${firstName} ${lastName}`
+// console.log(fullName);
+// console.log(`${firstName[0]} ${lastName[0]}`);
+
 //ikinci tapsiriq
 console.log(fullName.split(" ")[1], fullName.split(" ")[0]);
+
+// console.log(`${lastName} ${firstName}`)
 
 //Ucuncu tapsiriq
 let noSpace = fullName.replaceAll(" ", "");
